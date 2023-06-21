4eaa8fe5579ccb8a44510f5640551fd1326db8fe
dhkim0710 <137269918+dhkim0710@users.noreply.github.com>
2023-06-21 16:48:10 +0900

    Add files via upload

---
+++ Test Suites/Naver Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Naver Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>514ef8b8-5859-4488-87b3-425d6de6abe2</testSuiteGuid>
+   <testCaseLink>
+      <guid>fd4b57d9-23bd-4ddf-8a4e-68e917a8ec57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
